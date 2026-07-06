e45a48c79d330dd1a400138912d0b8b3c2033097
super-jihoon <dkswlgns0483@gmail.com>
2026-07-06 18:55:56 +0900

    Update 3_드롭다운.py

--- pages/3_드롭다운.py
+++ pages/3_드롭다운.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.header('드롭다운')
 
 option = st.selectbox(
-     '가장 좋아하는 색상은 무엇인가요?',
-     ('파랑', '빨강', '초록'))
+     '가장 좋아하는 야구팀은 무엇인가요?',
+     ('기아타이거즈', '삼성라이온즈', '한화이글스'))
 
-st.write('당신이 좋아하는 색상은 ', option)
+st.write('당신이 좋아하는 야구팀은 ', option)
